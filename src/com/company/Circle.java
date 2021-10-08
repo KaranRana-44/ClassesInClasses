755de913d136d4959bf5a7eca31c5dccba410191
Karanrana-44 <karanrana@billericak12.com>
2021-10-08 11:09:35 -0400

    Class-Sception

--- src/com/company/Circle.java
+++ src/com/company/Circle.java
@@ -3,5 +3,20 @@ package com.company;
 public class Circle {
     private double radius;
 
-    public Circle (double r,)
+    public Circle (double r){
+        radius= r;
+    }
+    public Circle (){
+        radius= 5;
+    }
+    public double area(){
+       double a = Math.PI * Math.pow(this.radius,2);
+       return a;
+    }
+    public double circumference(){
+        double c= 2* Math.PI * this.radius;
+        return c;
+    }
+
+
 }
--- src/com/company/Cylinder.java
+++ src/com/company/Cylinder.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Cylinder {
+    private Circle base;
+    private double height;
+
+    public Cylinder(double h, double r){
+        base= new Circle(r);
+        height = h;
+    }
+    public Cylinder(){
+        base= new Circle(1);
+        height = 2;
+    }
+    public double vol (){
+        return this.base.area()*this.height;
+    }
+    public double sa (){
+        return (this.base.area()*2)+(this.base.circumference()*this.height);
+    }
+
 }
